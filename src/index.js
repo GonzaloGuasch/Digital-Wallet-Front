5a6527d65e47eeb8f870efc4a51eab04900f14e6
ignacio <ignaciorobledounq@gmail.com>
2019-11-07 17:49:58 -0300

    Commit para mergear con master.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import 'bootstrap/dist/css/bootstrap.css'
-
+import 'semantic-ui-css/semantic.min.css'
 import App from "./js/App";
 
 ReactDOM.render(
+
+
     <App/>, document.getElementById('digitalWallet'
     )
 );
