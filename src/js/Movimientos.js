0684e6317ae3131fbf9ce77fcda52df1ca610e18
GonzaloGuasch <gonzaloguasch@outlook.com>
2019-11-11 14:20:36 -0300

    Moviento y movientos, check

---
+++ src/js/Movimientos.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './Movientos.css'
+import Movimiento from './Movimiento.js'
+import {movimientosDeCVU} from "./api";
+
+export default class Movimientos extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            cvu: props.cvu,
+            movientos: []
+        };
+    }
+
+    componentDidMount() {
+        const cvu = this.state.cvu;
+        let res = movimientosDeCVU({cvu: cvu}).then(res =>
+        {
+            console.log(res)
+            this.setState({movientos: res});
+        });
+    }
+
+    render(){
+        const misMovimientos = this.state.movientos.map(unMoviento => {
+            return(
+                <Movimiento movimiento= {unMoviento} />
+            )});
+
+        return(
+            <div>
+                <div className="Saldo-Container">
+                    <div>Saldo de:</div>
+                    <div className="Saldo-Money"> $300 </div>
+                </div>
+                <div className="Historial">Historial</div>
+                <div className="Historial-Cointeiner">
+                    <table className='Table-Cointeiner' width="100%" border="0">
+                        {misMovimientos}
+                    </table>
+                </div>
+            </div>
+        )};
+
+}
