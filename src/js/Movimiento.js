cfbd44f32cc8dc1dfbf52690aab2ccea27caf819
GonzaloGuasch <gonzaloguasch@outlook.com>
2019-11-13 18:24:36 -0300

    dias - meses - año de los movimientos

--- src/js/Movimiento.js
+++ src/js/Movimiento.js
@@ -13,7 +13,7 @@ class Movimiento extends React.Component{
         return(
             <div>
                 <div className="inner" id="Bottom-border">
-                    <div className="children">{this.state.movimiento.dateTime.dayOfWeek}</div>
+                    <div className="children">{this.state.movimiento.dateTime.dayOfMonth} - {this.state.movimiento.dateTime.monthValue} - {this.state.movimiento.dateTime.year}   </div>
                     <div className="children">{this.state.movimiento.description}</div>
                     <div className="children" id = "money">{this.state.movimiento.amount}</div>
                 </div>
